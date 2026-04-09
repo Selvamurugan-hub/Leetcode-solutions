406e3ad108c73868f9f67bbf96b5c280a9250ede
Selvamurugan S <selvamurugan0211@gmail.com>
2026-04-09 15:58:00 +0530

    Create 2. Add Two Numbers.java

---
+++ 2. Add Two Numbers.java
@@ -0,0 +1,22 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int val1 = (l1 != null) ? l1.val : 0;
+            int val2 = (l2 != null) ? l2.val : 0;
+
+            int total = val1 + val2 + carry;
+            carry = total / 10;
+            curr.next = new ListNode(total % 10);
+            curr = curr.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        return dummy.next;
+    }
+}
